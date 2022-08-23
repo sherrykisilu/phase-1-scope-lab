97ea817fc66371de4b18737f75d5fbfd192a5889
sherry <sherry.kisilu@studebnt.moringaschool.com>
2022-08-23 14:01:43 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName="bob"
+console.log(customerName);
+function upperCaseCustomerName(){
+    customerName=customerName.toUpperCase();
+     return customerName;
+}
+var bestCustomer;
+function setBestCustomer(){
+    return bestCustomer='not bob';
+}
+ function overwriteBestCustomer(){
+    bestCustomer="maybe bob";
+    return bestCustomer;
+}
+const leastFavoriteCustomer='thief';
+ function changeLeastFavoriteCustomer(){
+   return leastFavoriteCustomer="harriet";
+
+ }
